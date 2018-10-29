036da6d57474d6faa8a910e1ce6451a9deafe4e4
ykwimk <ykwimk@gmail.com>
2018-10-29 14:44:07 +0900

    modify

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function(){
+	
+	var btnChk = 0;
+
+	$(".search_btn").click(function(){
+		if(btnChk == 0){
+			$(".search_bg").fadeIn();
+			$(".search_wrap").slideDown();
+			btnChk = 1;
+		}else{
+			$(".search_bg").fadeOut();
+			$(".search_wrap").slideUp();
+			btnChk = 0;
+		}
+	});
+});
